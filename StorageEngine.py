025042948c8422d8ce3c0f3e8b845399dfebd92d
Giresse <giresseachouakong@gmail.com>
2023-05-22 08:52:42 -0400

    Initial files

---
+++ StorageEngine.py
@@ -0,0 +1,16 @@
+class StorageEngine:
+
+    def __init__(self) -> None:
+        self.path = "./"
+
+    def create_db(self, db_name):
+        pass
+
+    def db_get(self, db_name, item_key):
+        pass
+
+    def db_set(self, db_name, item):
+        pass
+
+    def db_remove(self, db_name, item_key):
+        pass
