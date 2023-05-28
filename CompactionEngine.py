8107fe56093558e486537ee2a379c20e56340f2a
Giresse <giresseachouakong@gmail.com>
2023-05-27 22:12:41 -0400

    adding compaction engine

---
+++ CompactionEngine.py
@@ -0,0 +1,7 @@
+class CompactionEngine:
+    def __init__(self) -> None:
+        pass
+
+    # This class is intended to compact the databse and reduce space taken due to item updates
+    def compact(self) -> None:
+        pass
