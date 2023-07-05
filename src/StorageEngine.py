71103719d8b4ecd9420b875b883ec8aa0c6db50f
Giresse <giresseachouakong@gmail.com>
2023-07-04 22:31:08 -0400

    rearranging folder structure

---
+++ src/StorageEngine.py
@@ -0,0 +1,51 @@
+import csv
+    
+class StorageEngine:
+
+    def __init__(self) -> None:
+        self.path = "./"
+
+    #  Take in headers as a list
+    def create_db(self, db_name, headers):
+        filename = self.path + db_name
+        with open(filename, 'w',newline = '') as csvfile:
+            writer = csv.DictWriter(csvfile , fieldnames=headers)
+            writer.writeheader()
+        
+    def db_get(self, db_name, item_key):
+        filename = self.path + db_name
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reversed(list(reader)):
+                if row[0] == item_key and row[1] != "Deleted":
+                    return row
+            
+        return -1
+
+    def db_set(self, db_name, item):
+        filename = self.path + db_name
+        headers = list(item.keys())
+        with open(filename, 'a', newline = '') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writerow(item)
+        print(item, " has been added to db")
+            
+
+    def db_remove(self, db_name, item_key):
+        filename = self.path + db_name
+        headers = [item_key]
+        with open(filename, 'a', newline = '') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow([item_key, 'Deleted'])
+            # There has got to be a more elegant way to write this
+        print(item_key, " has been deleted")
+    
+    
+if __name__ == "__main__":
+    storageEngine = StorageEngine()
+    storageEngine.create_db("testdb.csv", ["col1", "col2"])
+    storageEngine.db_set("testdb.csv", {"col1": "furstrow", "col2": "secondrow"})
+    storageEngine.db_set("testdb.csv", {"col1": "2row", "col2": "3drow"})
+    item = storageEngine.db_get("testdb.csv", "col1")
+    print("Result: ", item)
